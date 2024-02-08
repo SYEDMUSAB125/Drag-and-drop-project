cc83e6e9c3374852bf24d22cff0a85770bb218ef
Syed Musab <syedmasab125@gmail.com>
2024-02-08 16:00:08 +0500

    project completed

---
+++ index.js
@@ -0,0 +1,28 @@
+let lists = document.querySelectorAll(".list");
+let rightBox = document.querySelector(".right");
+let leftBox = document.querySelector(".left");
+
+for(listItem of lists){
+    listItem.addEventListener("dragstart", function(e){
+        let selected = e.target;
+        
+
+        rightBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        })
+        rightBox.addEventListener("drop", function(e){
+            rightBox.appendChild(selected)
+         
+            selected=null;
+        })
+
+        leftBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        })
+        leftBox.addEventListener("drop", function(e){
+            leftBox.appendChild(selected)
+            selected=null;
+        })
+
+    })
+}
